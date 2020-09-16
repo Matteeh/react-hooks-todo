178df22132372dbf2c5e9b8a2964c63697fadc45
mathias <mathias.rahikainen@gmail.com>
2020-09-16 23:47:09 +0300

    Add edit todo modal

---
+++ src/components/EditTodoModal/EditTodoModal.tsx
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from "react";
+import "./EditTodoModal.css";
+import { Button, Form, Modal } from "react-bootstrap";
+import TodoForm from "../TodoForm";
+
+export function EditTodoModal(props: any) {
+  const { title, oldTodo, onSubmit, ...rest } = props;
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredDescription, setEnteredDescription] = useState("");
+  const [enteredIsComplete, setIsComplete] = useState(false);
+
+  useEffect(() => {
+    if (oldTodo) {
+      setEnteredName(oldTodo.name);
+      setEnteredDescription(oldTodo.description);
+      setIsComplete(oldTodo.isCompleted);
+    }
+  }, [oldTodo.id]);
+
+  const cleanUponSubmit = () => {
+    setEnteredName("");
+    setEnteredDescription("");
+    setIsComplete(false);
+  };
+
+  return (
+    <Modal
+      {...rest}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header>
+        <Modal.Title>Selected Todo to edit: {title}</Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        <Form
+          id="todoForm"
+          className="todo-form"
+          onSubmit={(e) => {
+            e.preventDefault();
+            onSubmit(enteredName, enteredDescription, enteredIsComplete);
+            return cleanUponSubmit();
+          }}
+        >
+          <Form.Group controlId="formBasicText">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter name"
+              value={enteredName}
+              onChange={(e) => setEnteredName(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicText">
+            <Form.Label>Description</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Description"
+              value={enteredDescription}
+              onChange={(e) => setEnteredDescription(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group
+            controlId="formBasicCheckbox"
+            className="todo-form-check-grp"
+          >
+            <Form.Check
+              type="checkbox"
+              label="Completed"
+              checked={enteredIsComplete}
+              onChange={(e: any) => setIsComplete(e.target.value)}
+            />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+
+      <Modal.Footer>
+        <Button onClick={props.onHide} variant="secondary">
+          Close
+        </Button>
+        <Button type="submit" form="todoForm" variant="primary">
+          Submit edit
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -4,12 +4,18 @@ import TodoTable from "../TodoTable";
 import ConfirmationModal from "../ConfirmationModal";
 import { http } from "../../helpers/http";
 import { Todo } from "../../models/Todo";
+import EditTodoModal from "../EditTodoModal";
 
 const URL = "https://cosmostodosmicroservice.azurewebsites.net/api";
 
 export function Home(props: any) {
   const [todos, setTodos] = useState([] as any);
-  const [editTodo, setEditTodo] = useState({});
+  const [editTodo, setEditTodo] = useState({
+    id: "",
+    name: "",
+    description: "",
+    isComplete: false,
+  } as Todo);
   const [deleteTodo, setDeleteTodo] = useState({
     id: "",
     name: "",
@@ -19,10 +25,10 @@ export function Home(props: any) {
   const [displayConfirmationModal, setdisplayConfirmationModal] = useState(
     false
   );
+  const [displayEditTodoModal, setDisplayEditTodoModal] = useState(false);
 
   useEffect(() => {
     getTodos();
-    console.log(todos);
   }, []);
 
   const getTodos = () => {
@@ -36,21 +42,53 @@ export function Home(props: any) {
   };
 
   const handleOnConfirmDeleteClick = () => {
-    http.del(`${URL}/items/${deleteTodo.id}`).then((res) => {
-      setTodos((prevTodos: Todo[]) =>
-        prevTodos.filter((t: Todo) => t.id !== deleteTodo.id)
-      );
-      setDeleteTodo({
-        id: "",
-        name: "",
-        description: "",
-        isComplete: false,
+    http
+      .del(`${URL}/items/${deleteTodo.id}`)
+      .then((res) => {
+        setTodos((prevTodos: Todo[]) =>
+          prevTodos.filter((t: Todo) => t.id !== deleteTodo.id)
+        );
+        setDeleteTodo({
+          id: "",
+          name: "",
+          description: "",
+          isComplete: false,
+        });
+        setdisplayConfirmationModal(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setDeleteTodo({
+          id: "",
+          name: "",
+          description: "",
+          isComplete: false,
+        });
+        setdisplayConfirmationModal(false);
       });
-      setdisplayConfirmationModal(false);
-    });
   };
 
-  const handleOnEditClick = (todo: Todo) => {};
+  const handleOnEditClick = (todo: Todo) => {
+    setDisplayEditTodoModal(true);
+    setEditTodo(todo);
+    console.log("table edit clicked", todo);
+  };
+
+  const handleOnEditTodoModalSubmit = (
+    enteredName: string,
+    enteredDescription: string,
+    enteredIsComplete: boolean
+  ) => {
+    http
+      .put(`${URL}/items/${editTodo.id}`, {
+        name: enteredName,
+        description: enteredDescription,
+        enteredIsComplete,
+      })
+      .then((todo) => {
+        console.log(todo);
+      });
+  };
 
   /**
    * Handle Sign In Submit
@@ -89,6 +127,12 @@ export function Home(props: any) {
         onHide={() => setdisplayConfirmationModal(false)}
         title={`${deleteTodo.name}`}
         message={`Are you sure you want to delete ${deleteTodo.name}`}
+      />
+      <EditTodoModal
+        show={displayEditTodoModal}
+        oldTodo={editTodo}
+        onHide={() => setDisplayEditTodoModal(false)}
+        onSubmit={handleOnEditTodoModalSubmit}
       />
     </div>
   );
